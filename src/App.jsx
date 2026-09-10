adce93f79094a97ac43066e2be325be3f2ab7367
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-10 16:53:30 +0500

    the project has been stopped

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./publicPages/Header"
 import "./App.css"
-
+import Home from "./publicPages/Home"
 function App() {
 return <div>
   <Header/>
+  <Home/>
 </div>
   }
   export default App
